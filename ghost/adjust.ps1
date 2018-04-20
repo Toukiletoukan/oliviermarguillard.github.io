42c456209d82522ce6dfc20a17112ca3100d0c6b
Olivier Marguillard <omarguillard@scop-it.com>
2018-04-20 16:11:49 +0200

    remove ghost files

--- ghost/adjust.ps1
+++
@@ -1,7 +0,0 @@
-$files = Get-ChildItem . -recurse *.html
-foreach ($file in $files)
-{
-    (Get-Content $file.PSPath) |
-    Foreach-Object { $_ -replace "http://localhost:2368/", "https://toukiletoukan.github.io/" } |
-    Set-Content $file.PSPath
-}
